3b1bc27b5efc3be0afc6e62ba4e205972a91a804
SarahHamed <sarahhamed2496@gmail.com>
2019-03-11 23:01:10 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int in=0;
+    cin>>in;
+    vector<int> tableNum(in);
+    vector<int> binaryOut(in);
+    int flag=0;
+    int chosen=0;
+    int pos=0;
+
+    for (int i=0;i<in;i++)
+    {
+        scanf("%d",&tableNum[i]);
+    }
+
+    for(int i=0;i<in;i++)
+    {
+        for(int j=0;j<in;j++)
+        {
+            if(tableNum[i]==tableNum[j]&&(i!=j))
+                flag=1;
+        }
+    }
+    if (flag==1)
+    {
+        cout<<"IMPOSSIBLE"<<endl;
+    }
+    else
+        {
+
+        for(int i=0;i<in;i++)
+        {if(i>0)
+            cout<<" ";
+            chosen=in+1;
+            pos=i;
+                for(int j=0;j<in;j++)
+                {
+                 if(tableNum[j]<chosen&&(tableNum[j]!=0))
+                 {
+                    // cout<<"chosen= "<<chosen<<"tableNum= "<<tableNum[j]<<endl;
+                     chosen=tableNum[j];
+                     pos=j;
+                 }
+                }
+                //printf("%d ",pos+1);
+                cout<<pos+1;
+                tableNum[pos]=0;
+            }
+            cout<<endl;
+        }
+   /*     for(int i=0;i<in;i++)
+        {
+            cout<<tableNum[i]<<endl;
+
+        }*/
+    return 0;
+}
